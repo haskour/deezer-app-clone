27420245c6a730a4630289fa6689e976a3f975b3
hichamm <hishamaskour@gmail.com>
2020-12-19 14:47:18 +0100

    fix img alt warnings

--- src/components/ChanelSection.js
+++ src/components/ChanelSection.js
@@ -18,7 +18,7 @@ export default function ChanelSection() {
                 <div className="flex justify-between -mr-8">
                     <article className="w-1/4 pr-8">
                         <div className="relative shadow-md">
-                            <img className="rounded-md" src="https://e-cdns-images.dzcdn.net/images/cover/b3dd65f6ce5c6c32186bac6777471e6b/528x528-000000-80-0-0.jpg" />
+                            <img className="rounded-md" src="https://e-cdns-images.dzcdn.net/images/cover/b3dd65f6ce5c6c32186bac6777471e6b/528x528-000000-80-0-0.jpg" alt="" />
                             <div className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center">
                                 <button className="bg-white p-4 rounded-full">
                                     <svg focusable="false" height="1em" width="1em" viewBox="0 0 12 12" aria-hidden="true"><path fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M2.5.5v11l9-5.5z"></path></svg>
@@ -33,7 +33,7 @@ export default function ChanelSection() {
                     </article>
                     <article className="w-1/4 pr-8">
                         <div className="relative">
-                            <img className="rounded-md" src="https://e-cdns-images.dzcdn.net/images/artist/c2309ca87067cc282a054c5c6dd592cd-3c190a54f784989eeb7588f437aa36f5-0f9525cba3c8b4b5581d4c0586d4c771-76c17eefc3b4db330c416fab80e46132/528x528-000000-80-0-0.jpg" />
+                            <img className="rounded-md" src="https://e-cdns-images.dzcdn.net/images/artist/c2309ca87067cc282a054c5c6dd592cd-3c190a54f784989eeb7588f437aa36f5-0f9525cba3c8b4b5581d4c0586d4c771-76c17eefc3b4db330c416fab80e46132/528x528-000000-80-0-0.jpg" alt="" />
                             <div className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center">
                                 <button className="bg-white p-4 rounded-full">
                                     <svg focusable="false" height="1em" width="1em" viewBox="0 0 12 12" aria-hidden="true"><path fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M2.5.5v11l9-5.5z"></path></svg>
@@ -48,7 +48,7 @@ export default function ChanelSection() {
                     </article>
                     <article className="w-1/4 pr-8 relative">
                         <div className="relative">
-                            <img className="rounded-md" src="https://e-cdns-images.dzcdn.net/images/artist/b54bdf9fb6c34dd65509280f6601bb2b-0a583310e16929d26a6123a57f46786d-718942059d7b35b22a40da0d23554e8d-262e5d841121b9f998052360b9af6981/528x528-000000-80-0-0.jpg" />
+                            <img className="rounded-md" src="https://e-cdns-images.dzcdn.net/images/artist/b54bdf9fb6c34dd65509280f6601bb2b-0a583310e16929d26a6123a57f46786d-718942059d7b35b22a40da0d23554e8d-262e5d841121b9f998052360b9af6981/528x528-000000-80-0-0.jpg" alt="" />
                             <div className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center">
                                 <button className="bg-white p-4 rounded-full">
                                     <svg focusable="false" height="1em" width="1em" viewBox="0 0 12 12" aria-hidden="true"><path fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M2.5.5v11l9-5.5z"></path></svg>
@@ -63,7 +63,7 @@ export default function ChanelSection() {
                     </article>
                     <article className="w-1/4 pr-8 relative">
                         <div className="relative">
-                            <img className="rounded-md" src="https://e-cdns-images.dzcdn.net/images/artist/0f9525cba3c8b4b5581d4c0586d4c771-76c17eefc3b4db330c416fab80e46132-04cd7d2be87e209fa87e73010486b017-41640b69c6614e9b05cdd83dcf6131cc/528x528-000000-80-0-0.jpg" />
+                            <img className="rounded-md" src="https://e-cdns-images.dzcdn.net/images/artist/0f9525cba3c8b4b5581d4c0586d4c771-76c17eefc3b4db330c416fab80e46132-04cd7d2be87e209fa87e73010486b017-41640b69c6614e9b05cdd83dcf6131cc/528x528-000000-80-0-0.jpg" alt="" />
                             <div className="absolute top-0 right-0 bottom-0 left-0 flex justify-center items-center">
                                 <button className="bg-white p-4 rounded-full">
                                     <svg focusable="false" height="1em" width="1em" viewBox="0 0 12 12" aria-hidden="true"><path fillRule="evenodd" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M2.5.5v11l9-5.5z"></path></svg>
